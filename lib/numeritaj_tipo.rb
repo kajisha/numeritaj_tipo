339aa92eef2dbf923b4c84373947921f723c68cb
kajisha <kajisha@gmail.com>
2015-10-27 05:25:39 +0900

    add autoloads

--- lib/numeritaj_tipo.rb
+++ lib/numeritaj_tipo.rb
@@ -1,4 +1,16 @@
+require 'active_support'
 require 'numeritaj_tipo/version'
+require 'numeritaj_tipo/railtie' if defined?(Rails)
 
 module NumeritajTipo
+  extend ActiveSupport::Autoload
+
+  autoload :AttributeDefinition
+  autoload :Base
+  autoload :EnumBase
+  autoload :EnumType
+
+  autoload :ActiveRecord
+  autoload :Mongoid
+  autoload :Neo4j
 end
