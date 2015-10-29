0a4bfe21d30da1ae39b207a4a918a063d6a3e1b1
kajisha <kajisha@gmail.com>
2015-10-29 15:32:08 +0900

    update enums

--- spec/support/enums/payment_status.rb
+++ spec/support/enums/payment_status.rb
@@ -1,20 +1,5 @@
-class PaymentStatus < NumeritajTipo::EnumBase
-end
-
-class PaymentStatus::Pending < PaymentStatus
-  def initialize
-    @value = :pending
-  end
-end
-
-class PaymentStatus::Approved < PaymentStatus
-  def initialize
-    @value = :approved
-  end
-end
-
-class PaymentStatus::Declined < PaymentStatus
-  def initialize
-    @value = :declined
-  end
-end
+NumeritajTipo.define_enum :PaymentStatus, %i{
+  pending
+  approved
+  declined
+}
